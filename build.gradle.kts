485d92101ac44d7cba39ea15f890272b92e1be23
syhan <hsykys0728@gmail.com>
2026-03-23 00:32:48 +0900

    Spring boot 4.0은 h2 console 설정을 위한 코드 필요 -> 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-h2console")
 }
 
 kotlin {
